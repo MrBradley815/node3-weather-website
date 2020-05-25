f70456575d615198091ea11b07f3fa3f85d32d3d
MrBradley815 <pb_815@yahoo.com>
2020-05-25 18:24:29 -0500

    Add wind direction and speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,10 +9,13 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
+            console.log(body.current)
             const temperature = body.current.temperature
             const feelsLike = body.current.feelslike
             const description = body.current.weather_descriptions[0]
-            callback(undefined, `${description}. It is currently ${temperature} degrees out. It feels like ${feelsLike} degrees out.`)
+            const windDir = body.current.wind_dir
+            const windMph = body.current.wind_speed
+            callback(undefined, `${description}. It is currently ${temperature} degrees out. It feels like ${feelsLike} degrees out. Wind is coming from the ${windDir} at ${windMph} mph.`)
         }
     })
 }
